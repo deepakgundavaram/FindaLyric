30267f58a7aaf8ab323ad00f117545e18dfe3ec1
Matthew Leonard <leonard.matthewk@gmail.com>
2020-03-26 18:50:47 -0400

    update js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function clearSearch() {
-    $("#lyricSearch").val("");
-    $("#artistSearch").val("");
-    $("#lyrics").html("")
+  $("#lyricSearch").val("");
+  $("#artistSearch").val("");
+  $("#lyrics").html("");
 }
 
-
-
 // function getLyrics(){
-   
+
 //     var artistSearch = document.getElementById("artistSearch").value;
 //     var songSearch = document.getElementById("lyricSearch").value;
 //     console.log(songSearch);
-  
+
 //     $.ajax({
 //         type: "GET",
 //         data: {
@@ -20,15 +18,15 @@ function clearSearch() {
 //             q_track: songSearch,
 //             format:"jsonp",
 //             callback:"jsonp_callback",
-            
+
 //         },
 //         url: "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
 //         dataType: "jsonp",
 //         jsonpCallback: 'jsonp_callback',
 //         contentType: 'application/json',
 //         success: function(data) {
-//              console.log(data); 
-//             // var artist_id=  data.message.body.artist_list[0].artist.artist_id ;    
+//              console.log(data);
+//             // var artist_id=  data.message.body.artist_list[0].artist.artist_id ;
 //             var lyricsBody = data.message.body.lyrics.lyrics_body.split(/\s+/).slice(0,100).join(" ")+ "...";
 //             var j = document.createElement("p")
 //             j.textContent = lyricsBody
@@ -38,31 +36,53 @@ function clearSearch() {
 //             console.log(jqXHR);
 //             console.log(textStatus);
 //             console.log(errorThrown);
-//         } 
+//         }
 //     });
 
-    function getSongLyrics(){
-        var artist = document.getElementById("artistSearch").value;
-        var song = document.getElementById("lyricSearch").value;        
-            console.log(artist)
-        var apikey = "?apikey=HOaeWFw9AtiKvo2R3Ic98EFG1j6n4oobnVJDV2Q4K1pjWQKoV1lacK3KKvdYDi2J";
-        var queryURL = `https://orion.apiseeds.com/api/music/lyric/${artist}/${song}${apikey}`;
-        console.log(queryURL);
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function(response) {
-            var lyricsBody = response.result.track.text
-            var lyric = document.createElement("p")
-            lyric.classList.add("center-align")
-            lyricsBody = lyricsBody.replace(/\r\n/g, '<br />').replace(/[\r\n]/g, '<br />');
-            lyric.innerHTML= lyricsBody
-            console.log(response)
-            document.getElementById("lyrics").appendChild(lyric)           
-    });
-              
-};
-prepend
+function getSongLyrics() {
+  var artist = document.getElementById("artistSearch").value;
+  var song = document.getElementById("lyricSearch").value;
 
-lastfm = apikey 9f2163989bb8a45ce33782a837bb012a
+  var apikey =
+    "?apikey=HOaeWFw9AtiKvo2R3Ic98EFG1j6n4oobnVJDV2Q4K1pjWQKoV1lacK3KKvdYDi2J";
+  var queryURL = `https://orion.apiseeds.com/api/music/lyric/${artist}/${song}${apikey}`;
+  console.log(queryURL);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var lyricsBody = response.result.track.text;
+    var lyric = document.createElement("p");
+    lyric.classList.add("center-align");
+    lyricsBody = lyricsBody
+      .replace(/\r\n/g, "<br />")
+      .replace(/[\r\n]/g, "<br />");
+    lyric.innerHTML = lyricsBody;
+    console.log(response);
+    document.getElementById("lyrics").appendChild(lyric);
+  });
+  getArtistImage()
+}
+
+function getArtistImage() {
+    var artist = document.getElementById("artistSearch").value;
+    var apikey = "&apikey=0d990fVFAdY2XfKF2BvVRyUSgdJMCbReK6xWurMLBxsjSsVF2h06ADB0";
+    var queryURL = `https://api.happi.dev/v1/music?q=${artist}${apikey}`; 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+        // var artistImage = repsonse.result[0].cover;
+        // var image = document.createElement("img");
+        // image.innerHTML = artistImage;
+        // document.getElementById("artistPic").prepend(lyric);
+
+
+
+
+
+      });
+
+}
 
